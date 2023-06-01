4a99db169466eb3062a23258f01732d17fd9b653
Andres Jose Paternina Rubiano <apaterninar@gmail.com>
2023-06-01 13:48:46 -0500

    info

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,6 +2,12 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define('Product', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
     nombre: {
         type: DataTypes.STRING,
         allowNull: false,
